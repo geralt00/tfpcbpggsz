1eef27837b1924246f74888e281f059610221cec
Shenghui Zeng <shenghui.zeng@bristol.ac.uk>
2024-08-19 16:20:56 +0000

    add docs

--- tfpcbpggsz/core.py
+++ tfpcbpggsz/core.py
@@ -7,7 +7,6 @@ import numpy as np
 #from importlib.machinery import SourceFileLoader
 #from tfpcbpggsz.masspdfs import *
 #from tfpcbpggsz.phasecorrection import * #PhaseCorrection
-_PI = tf.constant(np.pi, dtype=tf.float64)
 
 class Module(object):
     pass
@@ -18,16 +17,5 @@ class Module(object):
 core = Module()
 
 
-def name_convert(decay_str='b2dk_LL_p'):
-    """
-    Convert the decay string to the correct name for the data file
-    """
-
-
-    decay_str = decay_str.split('_')[0]+'_'+decay_str.split('_')[1]+'_'+decay_str.split('_')[2]
-    if decay_str.split('_')[0] == 'b2dpi':
-        return 'DPi_KsPiPi_'+decay_str.split('_')[1]+'_'+decay_str.split('_')[2]
-    elif decay_str.split('_')[0] == 'b2dk':
-        return 'DK_KsPiPi_'+decay_str.split('_')[1]+'_'+decay_str.split('_')[2]
     
 
