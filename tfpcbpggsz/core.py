97fceab47c2f7c587d198a633dfa6d82714f4c6e
Shenghui Zeng <shenghui.zeng@bristol.ac.uk>
2024-08-19 17:07:20 +0000

    test deps

--- tfpcbpggsz/core.py
+++ tfpcbpggsz/core.py
@@ -4,8 +4,8 @@ import uproot as up
 import numpy as np
 import sys
 import iminuit
-from importlib.machinery import SourceFileLoader
-from tfpcbpggsz.masspdfs import *
+#from importlib.machinery import SourceFileLoader
+#from tfpcbpggsz.masspdfs import *
 from tfpcbpggsz.phasecorrection import * #PhaseCorrection
 
 class Module(object):
